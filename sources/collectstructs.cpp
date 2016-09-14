40766cf16c94a6377537c3235dc04b07e833ceec
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2016-09-14 18:51:26 +0300

    fix multidimensional array output

--- sources/collectstructs.cpp
+++ sources/collectstructs.cpp
@@ -1,4 +1,5 @@
 #include "collectstructs.h"
+#include "vvvptreehelper.hpp"
 using namespace clang;
 
 void addCommonFieldDecl(boost::property_tree::ptree& field,
@@ -16,19 +17,30 @@ void addCommonFieldDecl(boost::property_tree::ptree& field,
 void addArrayFieldDecl(boost::property_tree::ptree& field,
                        const clang::FieldDecl* decl)
 {
-    const auto& t = decl->getType();
+    auto t = decl->getType();
     if(!t->isConstantArrayType())
         return;
 
-    ConstantArrayType* ca = (ConstantArrayType*)t->getAsArrayTypeUnsafe();
-    const auto elemCount = ca->getSize();
-    const auto& elementTypeName = ca->getElementType().getAsString();
+    std::vector<uint64_t> arraySizeByDimensions;
+
+    while(t->isConstantArrayType()) {
+        ConstantArrayType* ca = (ConstantArrayType*)t->getAsArrayTypeUnsafe();
+        const auto elemCount = *ca->getSize().getRawData();
+        arraySizeByDimensions.push_back(elemCount);
+        t = ca->getElementType();
+    }
+
+    const auto& elementTypeName = t.getAsString();
     
     using boost::property_tree::ptree;
+    ptree arraySize;
+    for(const auto& s: arraySizeByDimensions)
+        ptree_array_add_values(arraySize, s);
+
     ptree arrayInfo;
-    arrayInfo.put("elemType", elementTypeName);
-    arrayInfo.put("elemCount", elemCount.toString(10, false));
-    field.push_back(std::make_pair("array", arrayInfo));
+    ptree_add_value(arrayInfo, "elemType", elementTypeName);
+    ptree_add_subnode(arrayInfo, "size", arraySize);
+    ptree_add_subnode(field, "array", arrayInfo);
 }
 
 void printStructDecls(clang::ASTContext& Context,
