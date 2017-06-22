77f9400ea7dae64ec24f724a75cab344031b33c0
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2017-06-22 20:27:41 +0300

    Remove unused code and files
    Remove old, complicated code

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,6 @@
 #include "arghelper.hpp"
-#include "flowchartbuilder.h"
 #include "structfuncinfocollector.hpp"
 #include "vvvclanghelper.hpp"
-#include "vvvsourcegraph.hpp"
 #include "vvvstlhelper.hpp"
 #include <boost/algorithm/string.hpp>
 #include <clang/Analysis/AnalysisContext.h>
@@ -10,21 +8,6 @@
 #include <clang/Lex/Preprocessor.h>
 #include <functional>
 #include <string>
-
-Graph createFlowChart(const clang::FunctionDecl* fdecl)
-{
-    Graph g;
-    const auto functionName = fdecl->getNameAsString();
-    auto start              = addFlowchartVertex(g, new VertexBegin(), 0,
-                                    functionName + "\n" + RUK_FUNCTION_BEGIN);
-    auto end = addFlowchartVertex(g, new VertexEnd(), 0,
-                                  functionName + "\n" + RUK_FUNCTION_END);
-    auto body =
-        getSemanticVertexFromStmt(fdecl->getBody(), g, fdecl->getASTContext());
-    auto bodyVertex = body->expand(start, end, end, end, end, 1);
-    boost::add_edge(start, bodyVertex, g);
-    return g;
-}
 
 void clangFlowChart(const clang::FunctionDecl* d)
 {
@@ -41,56 +24,7 @@ void clangFlowChart(const clang::FunctionDecl* d)
     // std::unique_ptr<CFG> fcfg = clang::CFG::buildCFG(d,d,Context);
 }
 
-void dumpGraph(clang::ASTContext& Context)
-{
-    const auto declsInMain         = getNonSystemDeclarations(Context);
-    const auto functionDeclsInMain = filterFunctions(declsInMain);
-    Graph bigGraph;
-    for (const auto& d : functionDeclsInMain) {
-        auto g = FunctionDescription(d).getFlowChart();
-        calculateBranchesnes(g);
-        boost::copy_graph(g, bigGraph);
-    }
-
-    //       boost::write_graphviz( std::cout, bigGraph, myLabler( bigGraph ),
-    //       myEdgeLabler(bigGraph), myGraphPropertyWriter() );
-
-    /*const auto recordDeclsInMain = filterStructs( declsInMain );
-      for( const auto& d : recordDeclsInMain )
-      printStructure( d );*/
-}
-
-void printFunctionsDecs(clang::ASTContext& Context)
-{
-    const auto declsInMain         = getNonSystemDeclarations(Context);
-    const auto functionDeclsInMain = filterFunctions(declsInMain);
-    for (const auto& d : functionDeclsInMain) {
-        std::cout << d->getReturnType().getAsString() << " ";
-        std::cout << d->getNameAsString() << std::endl;
-        for (const auto p : getFunctionParams(d))
-            std::cout << p->getNameAsString() << " : "
-                      << p->getType().getAsString() << ";" << std::endl;
-        std::cout << std::endl;
-    }
-}
-
-int printFlowChart(int argc, char** argv)
-{
-    const auto& args      = argstoarray(argc, argv);
-    const auto& params    = filterParams(args);
-    const auto& filenames = filterNotParams(args);
-
-    for (const auto& name : filenames) {
-        using namespace clang::tooling;
-        const auto code = getSourceFromFile(name.c_str());
-        runToolOnCodeWithArgs(new FlowChartAction(), code.c_str(), params,
-                              name.c_str());
-    }
-    return 0;
-}
-
 int main(int argc, char** argv)
 {
-    // return printFlowChart(argc,argv);
     return StructAndFuncInfoCollector(argc, argv);
 }
